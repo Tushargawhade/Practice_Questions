64455524dacee2e52a3df816c88e4b4c61af0de2
gawhade-tushar <148657863+Tushargawhade@users.noreply.github.com>
2025-09-03 16:19:52 +0530

    Time: 2 ms (77.26%) | Memory: 7.7 MB (87.24%) - LeetSync

---
+++ 1013-fibonacci-number/fibonacci-number.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int fib(int n) {
+        if (n==0){
+            return 0;
+        }
+        if (n==1) {
+            return 1;
+        }
+
+        int prev1= 0, prev2= 1, curr;
+
+        for (int i= 2; i<= n; i++) {
+            curr = prev1 + prev2;
+            prev1 = prev2;
+            prev2 = curr;
+        }
+        return prev2;
+    }
+};
