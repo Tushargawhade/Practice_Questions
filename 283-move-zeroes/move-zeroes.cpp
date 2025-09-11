f5b637ce3dce97ffa1192035bade8e94bf86599f
gawhade-tushar <148657863+Tushargawhade@users.noreply.github.com>
2025-09-11 22:34:10 +0530

    Time: 0 ms (100.00%) | Memory: 23.9 MB (55.36%) - LeetSync

---
+++ 283-move-zeroes/move-zeroes.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        int start = 0;
+
+        for(int i=0;i<nums.size();i++){
+            if(nums[i] != 0){
+                nums[start] = nums[i];
+                if(i != start){
+                    nums[i] = 0;
+                }
+                start++;
+            }
+        }
+    }
+};
