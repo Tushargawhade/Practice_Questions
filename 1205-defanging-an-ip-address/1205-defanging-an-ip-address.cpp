145c8976d5010a4bb488c773116027a725045bf7
gawhade-tushar <148657863+Tushargawhade@users.noreply.github.com>
2025-10-09 20:37:36 +0530

    Time: 3 ms (28.53%), Space: 7.9 MB (15.51%) - LeetHub

---
+++ 1205-defanging-an-ip-address/1205-defanging-an-ip-address.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    string defangIPaddr(string address) {
+        string new_address = "";
+
+        for(int i=0;i<address.size();i++){
+            if (address[i] == '.'){
+                new_address = new_address + "[.]";
+            }
+            else{
+                new_address = new_address + address[i];
+            }
+        }
+        return new_address;
+        
+    }
+};
